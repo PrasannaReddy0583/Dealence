c6f3265d87b089e5fc19127d8e52f02ef1029465
Prasanna Reddy <prasannareddynallamilli2666@gmail.com>
2025-09-30 20:12:14 +0530

    Ran flutter_launcher_icons:main

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class GradientScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: double.infinity,
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
